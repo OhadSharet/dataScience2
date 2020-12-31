bf05e9fba0428b85f1dca46a7720406c402aa76c
ohads <ohad.sharet@gmcil.com>
2020-12-31 22:41:34 +0200

    first commit

---
+++ ass2.py
@@ -0,0 +1,161 @@
+import numpy as np
+from loadMNIST import MnistDataloader, show_images
+from collections import Counter
+
+
+class Ex3():
+    BIT_SIZE = 255
+    K = 10
+    MAX_INT= 9223372036854775807
+
+
+    def __init__(self):
+        '''
+        @:param clusters(list) : list of clusters for each number
+        @:param group_rep (list) : list of group representative
+        '''
+        (self.img_train, self.img_test) = (None, None)
+        self.clusters =[[] for _ in range(self.K)]
+        self.load_data()
+        self.clusters_rep = self._init_cluster_centers_manually()
+        self.cluster_labels = [-1] * 10
+
+    def _init_cluster_centers_randomly(self):
+        ls = []
+        for _ in range(self.K):
+            ls.append(self._create_ranom_img())
+        return ls
+
+    def _init_cluster_centers_manually(self):
+        ls = [1,6,189,149,336,219,13,15,265,362]
+        ls_slim = [156,124,190,245,338,145,274,307,114,153]
+        return [self.img_train[index].img for index in ls]
+
+    def _create_ranom_img(self):
+        mat = [np.random.randint(2, size=28) for _ in range(28)]
+        return np.array(mat)
+
+    def run(self):
+        self._train_model()
+        self._test_model()
+
+    def _test_model(self):
+        print("testing model")
+        self.clusters = [[] for _ in range(self.K)]
+        self._devide_imgs_to_clusters(self.img_test)
+        self._print_result()
+
+    def _print_result(self):
+        print("printing result")
+        success_count_lst = self._measure_success()
+        labels_to_print = []
+        for i in range(self.K):
+            sucsess_rate = -1
+            if len(self.clusters[i])!=0:
+                sucsess_rate = success_count_lst[i] / len(self.clusters[i])
+            labels_to_print.append("clusrt '%s'|success '%s' " % (self.cluster_labels[i], format(sucsess_rate, ".3f")))
+        for st in labels_to_print:
+            print(st)
+        show_images(self.clusters_rep,labels_to_print)
+
+    def _measure_success(self):
+        sucsess_count = [0] * self.K
+        for i in range(self.K):
+            for img in self.clusters[i]:
+                if img.label == self.cluster_labels[i]:
+                    sucsess_count[i] += 1
+        return sucsess_count
+
+    def _devide_imgs_to_clusters(self, img_lst):
+        num_of_img_devided = 0
+        for img in img_lst:
+            index = self._get_closest_cluster_index(img)
+            self.clusters[index].append(img)
+
+            num_of_img_devided+=1
+            if num_of_img_devided%100==0:
+                print("num_of_img_devided %s: "%num_of_img_devided)
+
+    def _train_model(self):
+        counter = 1
+        while -1 in self.cluster_labels and counter < 20:
+            self._devide_imgs_to_clusters(self.img_train)
+            self.clusters_rep = [self._get_cluster_new_rep(i) for i in range(self.K)]
+            print("modole was trained for %s times"%counter)
+            counter+=1
+
+    def _get_cluster_new_rep(self,index):
+        if self.cluster_labels[index]!=-1:
+            return self.clusters_rep[index]
+        else:
+            rep = self._get_cluster_avg_rep(index)
+            if (rep == self.clusters_rep[index]).all():
+                self._fix_cluster_label_to_most_common(index)
+            return rep
+
+    def _get_cluster_avg_rep(self,index):
+        cluster_sum = np.zeros((28, 28))
+        for img in self.clusters[index]:
+            cluster_sum += img.img
+        cluster_avg = cluster_sum/len(self.clusters[index])
+        return cluster_avg
+
+    def _fix_cluster_label_to_most_common(self,index):
+        lst = [item.label for item in self.clusters[index]]
+        mc = self._most_common(lst)
+        self.cluster_labels[index] = mc
+        print("label of cluster %s was fixed to %s"%(index,mc))
+
+    def _most_common(self,lst):
+        if len(lst) == 0:
+            return -1
+        data = Counter(lst)
+        return data.most_common(1)[0][0]
+
+    def _get_closest_cluster_index(self, img):
+        i = 0
+        max_similarity_index = 0
+        max_similarity = -1
+        for rep in self.clusters_rep:
+            curr_similarity = self._similarity(rep, img.img)
+            if curr_similarity > max_similarity:
+                max_similarity_index = i
+                max_similarity = curr_similarity
+            i += 1
+        return max_similarity_index
+
+    def _update_clusters_rep(self):
+        pass
+
+    def _similarity(self, img1, img2):
+        '''
+
+        :param img1:
+        :param img2:
+        :return(int): a number thar represent how "close" img1 and img2 are
+        '''
+        sum_similarity = 0
+        for i in list(range(len(img1))):
+            for j in range(len(img1[i])):
+                img1_pixel = int((img1[i][j]))
+                img2_pixel = int((img2[i][j])/self.BIT_SIZE)
+                sum_similarity += abs(img1_pixel * img2_pixel)
+        return sum_similarity
+
+    def load_data(self):
+        mnist_dataLoader = MnistDataloader(MnistDataloader.TRAINING_IMAGES_FILEPATH,
+                                           MnistDataloader.TRAINING_LABELS_FILEPATH,
+                                           MnistDataloader.TEST_IMAGES_FILEPATH, MnistDataloader.TEST_LABELS_FILEPATH)
+        (img_train, label_train), (img_test, label_test) = mnist_dataLoader.load_data()
+        self.img_train = [Image(tup[0],tup[1]) for tup in list(zip(img_train, label_train))[:400]]
+        self.img_test = [Image(tup[0],tup[1]) for tup in list(zip(img_test, label_test))][:100]
+
+class Image():
+    def __init__(self,img,label):
+        self.img = img
+        self.label=label
+
+if __name__ == "__main__":
+    ex3 = Ex3()
+    ex3.run()
+    print("done")
---
+++ loadMNIST.py
@@ -0,0 +1,117 @@
+#
+# This is a sample Notebook to demonstrate how to read "MNIST Dataset"
+#
+import numpy as np
+import struct
+from array import array
+from os.path import join
+import os
+import random
+import matplotlib.pyplot as plt
+
+#
+# Set file paths based on added MNIST Datasets
+#
+
+
+
+#
+# MNIST Data Loader Class
+#
+class MnistDataloader(object):
+    cwd = os.getcwd()
+    INPUT_PATH = cwd + r'\MNIST'
+    TRAINING_IMAGES_FILEPATH = join(INPUT_PATH, r'files/train-images.idx3-ubyte')
+    TRAINING_LABELS_FILEPATH = join(INPUT_PATH, r'files/train-labels.idx1-ubyte')
+    TEST_IMAGES_FILEPATH = join(INPUT_PATH, r'files/t10k-images.idx3-ubyte')
+    TEST_LABELS_FILEPATH = join(INPUT_PATH, r'files/t10k-labels.idx1-ubyte')
+
+
+    def __init__(self, training_images_filepath, training_labels_filepath,
+                 test_images_filepath, test_labels_filepath):
+        self.training_images_filepath = training_images_filepath
+        self.training_labels_filepath = training_labels_filepath
+        self.test_images_filepath = test_images_filepath
+        self.test_labels_filepath = test_labels_filepath
+
+    def read_images_labels(self, images_filepath, labels_filepath):
+        labels = []
+        with open(labels_filepath, 'rb') as file:
+            magic, size = struct.unpack(">II", file.read(8))
+            if magic != 2049:
+                raise ValueError('Magic number mismatch, expected 2049, got {}'.format(magic))
+            labels = array("B", file.read())
+
+        with open(images_filepath, 'rb') as file:
+            magic, size, rows, cols = struct.unpack(">IIII", file.read(16))
+            if magic != 2051:
+                raise ValueError('Magic number mismatch, expected 2051, got {}'.format(magic))
+            image_data = array("B", file.read())
+        images = []
+        for i in range(size):
+            images.append([0] * rows * cols)
+        for i in range(size):
+            img = np.array(image_data[i * rows * cols:(i + 1) * rows * cols])
+            img = img.reshape(28, 28)
+            images[i][:] = img
+
+        return images, labels
+
+    def load_data(self):
+        img_train, lable_train = self.read_images_labels(self.training_images_filepath, self.training_labels_filepath)
+        img_test, lable_test = self.read_images_labels(self.test_images_filepath, self.test_labels_filepath)
+        return (img_train, lable_train), (img_test, lable_test)
+
+    #
+
+
+# Verify Reading Dataset via MnistDataloader class
+#
+
+
+#
+# Helper function to show a list of images with their relating titles
+#
+def show_images(images, title_texts):
+    cols = 5
+    rows = int(len(images) / cols) + 1
+    plt.figure(figsize=(30, 20))
+    index = 1
+    for x in zip(images, title_texts):
+        image = x[0]
+        title_text = x[1]
+        plt.subplot(rows, cols, index)
+        plt.imshow(image, cmap=plt.cm.gray)
+        if title_text != '':
+            plt.title(title_text, fontsize=15);
+        index += 1
+    plt.show()
+
+def test_main():
+    #
+    # Load MINST dataset
+    #
+    mnist_dataloader = MnistDataloader(MnistDataloader.TRAINING_IMAGES_FILEPATH, MnistDataloader.TRAINING_LABELS_FILEPATH,
+                                       MnistDataloader.TEST_IMAGES_FILEPATH, MnistDataloader.TEST_LABELS_FILEPATH)
+    (img_train, lable_train), (img_test, lable_test) = mnist_dataloader.load_data()
+
+    #
+    # Show some random training and test images
+    #
+    images_2_show = []
+    titles_2_show = []
+    for r in range(350,400):
+        #r = random.randint(1, 60000)
+        images_2_show.append(img_train[r])
+        titles_2_show.append('training image [' + str(r) + '] = ' + str(lable_train[r]))
+
+    for i in range(0, 5):
+        r = random.randint(1, 10000)
+        images_2_show.append(img_test[r])
+        titles_2_show.append('test image [' + str(r) + '] = ' + str(lable_test[r]))
+
+    show_images(images_2_show, titles_2_show)
+
+
+if __name__ == "__main__":
+   test_main()
